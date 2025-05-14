dd09e1597e754b5db3579e5632d52ecccd9fdd75
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-05-14 15:18:26 +0300

    adjusting the server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require('./app')
 dotenv.config({path: './config.env'});
 
 //initializing the port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = app.listen(port, ()=>{
     console.log(`app running on ${port}`);
 })
